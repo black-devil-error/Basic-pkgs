5d099b8ef7166b12c69de7d52fd543831c37f583
C_matrix-BOY <68942604+black-devil-error@users.noreply.github.com>
2020-12-05 18:25:33 +0530

    Create tools.sh

---
+++ tools.sh
@@ -0,0 +1,64 @@
+
+echo ""
+echo ""
+echo ""
+echo "The script is  made by Black-Devil-error"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Installtion process is started" 
+echo ""
+echo ""
+echo ""
+
+apt-get update -y                    
+apt install python -y           
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+apt install screenfetch -y
+apt install cowsay -y
+apt install toilet -y
+apt install figlet -y
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+pkg install openssh -y
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install fish -y
+apt install unzip -y
+apt install tor -y 
+apt install sudo -y
+apt install wget -y
+apt install tor -y
+apt install w3m -y
+apt install cmatrix -y
+apt install wcalc -y 
+apt install openssl -y
+apt install bmon -y
+apt-get upgrade -y
+echo ""
+echo ""
+echo ""
+echo ""
+
+echo "All basic packages and tools is installed"
+echo ""
+echo ""
+echo "Be a Hacker...."
+echo ""
+echo ""
+echo "join my discord server"
+echo "https://discord.gg/rb4f7H5"
+echo ""
+echo ""
+exit
